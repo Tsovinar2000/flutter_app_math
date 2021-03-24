9e2c363ce6cc4054098419c1265c77a0f628dfa3
Tsovinar2000 <tsov.vardanyan2000@gmail.com>
2021-03-24 16:10:48 +0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,184 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app_math/brain.dart';
+
+
+QuizBrain quizBrain = QuizBrain();
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  String answer = '';
+
+  void checkAnswer(String userPickedAnswer) {
+    String correctAnswer = quizBrain.getCorrectAnswer();
+
+    setState(() {
+      if (quizBrain.isFinished() == true) {
+        quizBrain.reset();
+        scoreKeeper = [];
+      }
+
+      else {
+        if (userPickedAnswer == correctAnswer) {
+          scoreKeeper.add(Icon(
+            Icons.check,
+            color: Colors.white,
+            size: 35.0,
+
+
+
+          ));
+        } else {
+          scoreKeeper.add(Icon(
+            Icons.close,
+            color: Colors.red,
+            size: 35.0,
+          ));
+        }
+        quizBrain.nextQuestion();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        image: DecorationImage(image: AssetImage("images/bcg.jpg"), fit: BoxFit.cover),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            //flex: 5,
+            child: Padding(
+              padding: EdgeInsets.all(5),
+              child: Center(
+                child: Text(
+                  quizBrain.getQuestionText(),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Row(
+            children: <Widget>[
+
+              _images(1),
+              _images(2),
+              _images(3),
+
+            ],
+          ),
+
+          Row(
+            children: <Widget>[
+              _images(4),
+              _images(5),
+              _images(6),
+
+
+            ],
+          ),
+
+          Row(
+            children: <Widget>[
+              _images(7),
+
+              _images(8),
+              _images(9),
+            ],
+          ),
+          _images(0),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: FlatButton(
+                color: Colors.green,
+                child: Text(
+                  'Ստուգել',
+                  style: TextStyle(
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                onPressed: () {
+                  //The user picked false.
+                  checkAnswer(answer);
+                  answer="";
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(4.0),
+              child: FlatButton(
+                color: Colors.green,
+                child: Text(
+                  '$answer',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+
+              ),
+            ),
+          ),
+          Row(
+            children: scoreKeeper,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _images(int imageIndex) =>
+      Expanded(
+        child: Padding(
+          padding: EdgeInsets.only(bottom: 5),
+          child: FlatButton(
+            textColor: Colors.white,
+            //color: Colors.green,
+            child: Image.asset('images/num$imageIndex.jpg'),
+            onPressed: () {
+              answer += "$imageIndex";
+            },
+          ),
+        ),
+      );
+}
